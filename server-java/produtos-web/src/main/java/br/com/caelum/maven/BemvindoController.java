d2a4f829b8f1ec509d4782f3974606c1017bbe62
Alessandro T. Santos <alessandro.teixeira@gmail.com>
2016-05-12 18:40:56 -0300

    Atualizando um primeiro modelo de projeto java pra ser utilizado com o angular2

---
+++ server-java/produtos-web/src/main/java/br/com/caelum/maven/BemvindoController.java
@@ -0,0 +1,19 @@
+package br.com.caelum.maven;
+
+
+//@Resource
+public class BemvindoController {
+
+//    private Result result;
+//
+//    public BemvindoController(Result result) {
+//        this.result = result;
+//    }
+//
+//    @Path("/")
+//    public void bemvindo() {
+//        System.out.println("estou invocando o vraptor");
+//        result.use(Results.http()).body("<html>bem vindo!</html>");
+//    }
+
+}
