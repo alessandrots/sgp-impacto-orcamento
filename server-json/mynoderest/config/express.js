2c308c36b4d65cc9ecee69f9ba624a5dc611d937
Alessandro T. Santos <alessandro.teixeira@gmail.com>
2016-05-16 13:25:12 -0300

    Atualizando o cors no servidor para o node/json.

--- server-json/mynoderest/config/express.js
+++ server-json/mynoderest/config/express.js
@@ -6,6 +6,14 @@ var express = require('express')
 
 app.use(bodyParser.json());
 
+// headers para permitir CORS
+ app.use(function(req, res, next) {
+     res.header('Access-Control-Allow-Origin', '*');
+     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+     next();
+ });
+
 routes(app);
 routesSgp(app);
 
