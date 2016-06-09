c8d9e8f108450177d2747eca2dc0d8fc86dd2494
Alessandro Teixeira <alessandro.teixeira@gmail.com>
2016-06-09 17:53:30 -0300

    Subindo o projeto oficial, preparando para montar a estrutura correta.

---
+++ server-java/ImpactoOrcamentoSgpMPU/src/main/java/br/mp/mpf/impactoorcamento/sgp/App.java
@@ -0,0 +1,13 @@
+package br.mp.mpf.impactoorcamento.sgp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
