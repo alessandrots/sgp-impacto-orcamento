24a4af32c77a8b0113d1c513f7e2dd0237cd2e10
Alessandro Teixeira Santos <alessandro.teixeira@gmail.com>
2016-07-07 14:30:10 -0300

    Atualizando a solução que chama o impacto financeiro a partir de datas.

--- angular2/newimpactoorcamentario/src/app/components/impacto2/impacto-inicial.ts
+++ angular2/newimpactoorcamentario/src/app/components/impacto2/impacto-inicial.ts
@@ -12,13 +12,26 @@ import {
   FORM_DIRECTIVES,
   FORM_PROVIDERS,
   FormBuilder,
-  Validators
+  Validators,
+  NgFor
 } from '@angular/common';
+
+import {PaginatePipe,
+  PaginationControlsCmp,
+  PaginationService}
+  from '../../../../node_modules/ng2-pagination';
+
+
+interface IServerResponse {
+    items: ConcursadoModel[];
+    total: number;
+}
 
 @Component({
   selector: 'orc-impacto-inicial-page',
-  providers: [FORM_PROVIDERS],
-  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
+  providers: [FORM_PROVIDERS, PaginationService],
+  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, NgFor,PaginationControlsCmp],
+  pipes: [PaginatePipe],
   template: require('./impacto-inicial.html')
 })
 
@@ -26,58 +39,77 @@ export default class ImpactoInicialComponente2 {
 
   formModel: ControlGroup;
   mainService: ConcursadoService;
+  result: Observable<ConcursadoModel[]>;
   @Input('data') resultAll: ConcursadoModel[] = [];
+  impacto: Concursado;
+  total: number;
+  p: number = 1;
+  loading: boolean;
 
   constructor(http: Http,  private _router: Router, mainService: ConcursadoService) {
-    // console.log('ConcursoRemocaoComponente Construtor');
     var me = this;
-    // this.impacto = new ConcursoRemocaoModel();
     me.mainService = mainService;
-    // this.categories = ['A','B','C','D'];
+    this.total = 0;
 
     const fb = new FormBuilder();
     this.formModel = fb.group({
       'dataInicial': [null, Validators.required],
       'dataFinal': [null, Validators.required]
     });
-    // this.formModel = fb.group({
-    //   'dataRemocaoGravacao': ['', Validators.required]
-    // });
-
-    //SUBSTITUINDO por chamada ao serviço
-    // mainService
-    //   .getDatasRemocoes()
-    //   .subscribe(
-    //     data => {
-    //       this.resultAll = data;
-    //       this.total = data.length;
-    //       me.getPage(1);
-    //     },
-    //     error => console.error(error));
   }
 
   onSearch() {
     let dataInicial, dataFinal;
+    var me=this;
+
     if (this.formModel.valid) {
       // this.mainService.searchEvent.emit(this.formModel.value);
-      console.log('this.formModel = ', this.formModel);
+      // console.log('this.formModel = ', this.formModel);
       this.mainService
         .getConcursadosPorDatas(this.formModel._value.dataInicial,
                                 this.formModel._value.dataFinal)
         .subscribe(
           data => {
             this.resultAll = data;
-            // this.total = data.length;
-            // me.getPage(1);
-
             console.log('this.resultAll = ', this.resultAll);
+            this.total = data.length;
+            me.getPage(1);
           },
           error => console.error(error));
       console.log('this.mainService.searchEvent = ', this.mainService.searchEvent);
     }
   }
 
+  /**
+   * Faz a lógica de paginação.
+   */
+  serverCall(meals: ConcursadoModel[], page: number): Observable<IServerResponse> {
+      const perPage = 10;
+      const start = (page - 1) * perPage;
+      const end = start + perPage;
+
+      return Observable
+          .of({
+              items: meals.slice(start, end),
+              total: meals.length
+          });//.delay(1000);
+  }
+
+  getPage(page: number) {
+      this.loading = true;
+      // console.log('ConcursadoComponente ::: tamanho array  = ', this.resultAll.length);
+      // console.log('ConcursadoComponente ::: page: number = ', page);
+      this.result = this.serverCall(this.resultAll, page)
+        .do(res => {
+            // console.log('getPage = ', res);
+            this.total = res.total;
+            this.p = page;
+            this.loading = false;
+        })
+        .map(res => res.items);
+  }
 }
+
 
 function positiveNumberValidator(control: Control): any {
   if (!control.value) return null;
